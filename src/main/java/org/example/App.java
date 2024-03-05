490c7f50e43d445c880e969fc0ad812bba40b910
isakovero <isakovero@gmail.com>
2024-03-05 19:54:38 +0200

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class App {
     public static void main(String[] args) {
         // Fahrenheit to Celsius
         double fahrenheit = 98.6;
